68d46672a83e0cee60ca127949e430b36aad1719
kearmododragon <ckearney1992@gmail.com>
2023-05-02 23:00:55 +0200

    fixed bug, added notes and played with info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return <h1>Instagram | CV | Linkedin |  </h1>;
+    
   }
   
   export default Footer;
--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,7 @@ function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getAboutData(), []);
+  useEffect(() => {getAboutData()}, []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
@@ -26,6 +26,8 @@ function About(props) {
       <h2>{about.name}</h2>
       <h3>{about.email}</h3>
       <p>{about.bio}</p>
+      <p>{about.Podcast}</p>
+      <p>{about.dog}</p>
     </div>
   );
 
