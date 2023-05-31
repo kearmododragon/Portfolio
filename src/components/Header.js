673897bc7bde3dce044f42dce73c71313f42fe27
kearmododragon <ckearney1992@gmail.com>
2023-05-31 23:08:18 +0200

    removed links, updated backend however that's not updating on front end

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,9 @@
-import { Link } from "react-router-dom";
-
 function Header(props) {
   //inline style for the nav tag
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "3px solid black",
-    padding: "8px",
-    width: "90%",
-    margin: "auto",
-  };
 
   return (
     <header>
       <h1>Welcome to my portfolio.</h1>
-      <nav style={navStyle}>
-        <Link to="/">
-          <div className="link">HOME</div>
-        </Link>
-        <Link to="/about">
-          <div className="link">Personal life</div>
-        </Link>
-        <Link to="/projects">
-          <div className="link">PROJECTS</div>
-        </Link>
-        <Link to="/working-career">
-          <div className="link">working career</div>
-        </Link>
-        <Link to="/">
-          <div className="link">contact(nl)</div>
-        </Link>
-      </nav>
     </header>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,11 +23,7 @@ function About(props) {
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
     <div>
-      <h2>{about.name}</h2>
-      <h3>{about.email}</h3>
       <p>{about.bio}</p>
-      <p>{about.Podcast}</p>
-      <p>{about.dog}</p>
     </div>
   );
 
