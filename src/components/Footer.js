2991cfcce93fa3f58b0f69b73c257cf98329b3b8
kearmododragon <ckearney1992@gmail.com>
2023-06-03 23:45:39 +0200

    footer 100% header still tbc

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,34 @@ import LinkedinIcon from "../Storage/Icons/linkedin.png";
 function Footer(props) {
   return (
     <section className="Footer">
+      <p className="Footer-text" style={{ height: "45px", position: "fixed", bottom: "30px", right: "30px" }}>Ciaran Kearney</p>
       <a href="https://www.instagram.com/kearmododragon/" target="_blank" rel="noopener noreferrer">
-        <img src={instagramIcon} alt="Instagram" />
+        <img
+          className="Footer-icon"
+          src={instagramIcon}
+          alt="Instagram"
+          style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "10px" }}
+        />
       </a>
       <a href="https://docs.google.com/document/d/1ayxaItR3ojyBvfQQWqcHQ73ov_Bm6-6t/export?format=pdf" target="_blank" rel="noopener noreferrer">
-        <img src={CVIcon} alt="CV" />
+        <img
+          className="Footer-icon"
+          src={CVIcon}
+          alt="CV"
+          style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "60px" }}
+        />
       </a>
       <a href="https://www.linkedin.com/in/ciarankearney92/" target="_blank" rel="noopener noreferrer">
-        <img src={LinkedinIcon} alt="LinkedIn" />
+        <img
+          className="Footer-icon"
+          src={LinkedinIcon}
+          alt="LinkedIn"
+          style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "110px" }}
+        />
       </a>
     </section>
   );
 }
+
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,17 @@ function Header(props) {
 
   return (
     <header>
-    <a href="#Home">Home</a>
-    <a href="#About">About Me</a>
-    <a href="#Projects">Projects</a>
+<a href="#Home" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "100px" }}>
+  Home
+</a>
+<a href="#About" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "200px" }}>
+  About Me
+</a>
+<a href="#Projects" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "300px" }}>
+  Projects
+</a>
+
+
     </header>
   );
 }
