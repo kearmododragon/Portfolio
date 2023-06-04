03b42523473942febd1fe4518e2b465938ce86a4
kearmododragon <ckearney1992@gmail.com>
2023-06-04 21:06:41 +0200

    box sorted (further scope), footer text in white

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,22 @@ function Header(props) {
   //inline style for the nav tag
 
   return (
-    <header style={{ backgroundColor: "blue" }}>
-<a href="#Home" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "100px" }}>
-  Home
-</a>
-<a href="#About" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "200px" }}>
-  About Me
-</a>
-<a href="#Projects" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "300px" }}>
-  Projects
-</a>
+   
+      <header style={{ backgroundColor: "blue" }}>
+ <div className="header">
+        <a href="#Home" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "100px" }}>
+          Home
+        </a>
+        <a href="#About" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "200px" }}>
+          About Me
+        </a>
+        <a href="#Projects" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "300px" }}>
+          Projects
+        </a>
 
-
-    </header>
-  );
+</div>
+      </header>
+      );
 }
 
 export default Header;
