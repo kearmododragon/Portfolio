a9064099fb3a6001613de7944b03c8b1a685cac5
kearmododragon <ckearney1992@gmail.com>
2023-08-10 15:05:14 +0200

    fixed header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import instagramIcon from "../Storage/Icons/instagram2.png";
-import CVIcon from "../Storage/Icons/CV2.png";
 import LinkedinIcon from "../Storage/Icons/linkedin2.png";
 import '../App.css';
 
@@ -8,14 +7,14 @@ function Footer(props) {
   return (
 
     <footer className="footer">
-    <div className="footer-content">
+      <div className="footer-content">
         <p className="Footer-text" style={{ height: "45px", position: "fixed", bottom: "30px", right: "30px" }}>Ciaran Kearney</p>
         <a href="https://www.instagram.com/kearmododragon/" target="_blank" rel="noopener noreferrer">
           <img
             className="Footer-icon"
             src={instagramIcon}
             alt="Instagram"
-            style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "10px" }}
+            style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "30px" }}
           />
         </a>
         <a href="https://www.linkedin.com/in/ciarankearney92/" target="_blank" rel="noopener noreferrer">
@@ -23,10 +22,10 @@ function Footer(props) {
             className="Footer-icon"
             src={LinkedinIcon}
             alt="LinkedIn"
-            style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "110px" }}
+            style={{ width: "40px", height: "40px", position: "fixed", bottom: "10px", right: "95px" }}
           />
         </a>
-        </div>
+      </div>
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,14 @@
 import '../App.css';
 function Header(props) {
-  return (
-   
-      <header className='header'>
- <div className="header-content">
-        <a className="hello" href="#Projects" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "179px" }}>
-          Projects
-        </a>
-        <a className="hello" href="#About" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "100px" }}>
-          About Me
-        </a>
-        <a className="hello" href="#Home" style={{ position: "fixed", top: "10px", right: "10px", marginRight: "50px" }}>
-          Home
-        </a>
+  return ( 
+<header className='header'>
+  <div className="header-content">
+    <a className="hello" href="#Projects">Projects</a>
+    <a className="hello" href="#About">About Me</a>
+    <a className="hello" href="#Home">Home</a>
+  </div>
+</header>
 
-</div>
-      </header>
       );
 }
 
