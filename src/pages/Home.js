6477de247638c056626513b6f0c085e5b333f4cb
kearmododragon <ckearney1992@gmail.com>
2023-08-10 15:33:19 +0200

    Updated about me section and formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,14 +45,21 @@ function About(props) {
       <div id="About">
         <p className="titles">About me.</p> </div>
       <section className="bio">
-        <p>----- THIS IS NOT TO BE KEPT THIS WAS JUST NOTES I WAS TAKING
-          ----- Below you will have access to my previous work, all completed while studying at General Assembley. I currently work for Mercedes-Benz as a Logistics Specialist however I want to follow my passion and am looking to take a leap into the world of Software Engineering. I have a dog named {about.dog} and she takes up most of my life now. She's a black Galgo and I love her to pieces. I play football in my spare time and when i'm feeling less active like to relax by playing video games, original I know.  Ciaran Kearney graduated from General Assembley with a qualification in May 2023. He enjoys working in full-stack developments and looks forward to improving his skills in order to both excell at his chosen career and also to be able to work on passion projects.</p>
-          <a>ADD IN THIS SECITON THE SKILLS, MAYBE AS IMAGES</a></section>
+        <p> I'm a 30 year old who studied software engineering in 2023. It's something i've long wanted to do and i'm proud of how much I got done. I did the course while working full time which had it's own challenges but thanks to my support network I made it through.</p>
+
+        <p>I'm still working on my skill level, which will be reflected in the ever changing nature of my potfolio. This isn't a CV though, I just want to let you know who I am.</p>
+
+        <p>Just a few days before I started my course, my partner and I adopted a dog! {about.dog} was rescued from a shelter in Spain and she's our princess. She's a galgo, she's almost 2 years old and is equal parts really fast and fast asleep.</p>
+
+        <p>At the weekends I play football for R.K.H.S.V Heer in Maastricht. I'm entering my third season with the team having won the league in my first season, getting promoted and finishing second the next. I covered in goal during the end of the season and managed to pick up "Goalkeeper of the season" at the end of year awards. We are looking forward to our first season in the 4th Klasse.</p>
+
+        <p>Beyond sport, work and my family I love to travel. Whether it's a solo trip, visiting friends of travelling with my partner getting away from where I'm based for a while is unbeatable. I've currently travelled to 40 different countries and i'm not looking to stop. My most recent trip was to Canada and the United states. in 2 weeks I flew to Toronto to watch the Maple Leafs play and see Niagra falls, then a flight to Chicago to explore the windy city. Next a drive to Cincinnati with a stop off at Casey to see the worlds largest rocking chair. I spent my birthday in Cincinnati watching the Bengals beat the Falcons before ending my trip with a few days in New York eating $1 pizza's. Next stop; Wales.</p>
+      </section>
       <div id="Projects">
         <p className="titles">Projects.</p></div>
       <div className="container">
         <div className="columns is-multiline">
-        {projects !== null && projects.map((project) => (
+          {projects !== null && projects.map((project) => (
             <div key={project.id} className="column is-one-third">
               <div className="card">
                 <div className="card-content">
@@ -72,7 +79,7 @@ function About(props) {
           ))}
         </div>
       </div>
-    </div>
+    </div >
 
   );
   return about ? loaded() : <h1>Loading...</h1>;
